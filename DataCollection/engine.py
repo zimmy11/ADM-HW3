19d05cbebf7e53a2a9f248c25f80d594977d0d18
zimmy11 <zimmatore.1947442@studenti.uniroma1.it>
2024-11-09 21:04:13 +0100

    Development of Quesntion 2.2

--- DataCollection/engine.py
+++ DataCollection/engine.py
@@ -4,7 +4,10 @@ from DataCollection.organize_folders import organize_folders
 import time
 import logging
 import asyncio
-
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import math
 
 def run_pipeline():
 
@@ -46,3 +49,83 @@ def run_pipeline():
     logging.info(f"Total parsing time: {time.time() - start_time} seconds") # <3
 
 #run_pipeline()
+
+def compute_tfidf(df, inverted_index):
+    total_documents = df.shape[0]
+    for term_id, documents in inverted_index.items():
+        documents_frequency = math.log(total_documents / (1 + len(documents)))
+        idf_scores = []
+
+        for i in range(len(documents)):
+            doc_id = documents[i]
+            doc = df.iloc[int(doc_id)]["description"]
+            tf_score = doc.count(term_id) / (len(doc.split()))
+            idf_score = documents_frequency * tf_score
+            idf_scores.append((doc_id, idf_score))
+        inverted_index[term_id] = idf_scores
+    return inverted_index
+
+def cosine_similarity(query, document):
+    product = 0
+    for score in query:
+        for index in document:
+            if score[0] == index[0]:  
+                product += score[1] * index[1]
+        
+    norm1 = math.sqrt(sum(val[1] ** 2 for val in query))
+    norm2 = math.sqrt(sum(val[1] ** 2 for val in document))
+    
+    if norm1 == 0 or norm2 == 0:
+        return 0  
+    return product / (norm1 * norm2)
+
+
+def compute_tfidf_query(query, inverted_index, df, vocabulary):
+    total_documents = df.shape[0]
+    tfidf_scores = []
+
+    for word in query.split():
+        term_id = str(vocabulary[vocabulary["word"] == word]["term_id"].iloc[0])
+        tf = query.count(word) / (len(query.split()))
+        idf = math.log(total_documents / 1 + len(inverted_index[term_id]))
+        tf_idf = tf * idf
+        tfidf_scores.append((term_id,tf_idf))
+
+    return tfidf_scores
+
+def rank_documents(query, inverted_index, df, vocabulary):
+    query_tfidf_scores = compute_tfidf_query(query, inverted_index, df, vocabulary)
+
+
+    document_tfidf = {}
+    term_ids = [x[0] for x in query_tfidf_scores]
+    for word in term_ids:
+
+        for entry in inverted_index[word]:
+            doc_id = entry[0]
+            tf_idf = entry[1]
+
+            if doc_id not in document_tfidf:
+                document_tfidf[doc_id] = []
+            document_tfidf[doc_id].append((word, tf_idf))
+    print(document_tfidf)
+    print(query_tfidf_scores)
+    similarities = []
+    for doc_id, doc_vector in document_tfidf.items():
+        sim = cosine_similarity(query_tfidf_scores, doc_vector)
+        similarities.append((doc_id, sim))
+
+
+    ranked_documents = sorted(similarities, key=lambda x: x[1], reverse=True)
+    
+    return ranked_documents
+   
+
+
+
+
+
+
+    
+
+
